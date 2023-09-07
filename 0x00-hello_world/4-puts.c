1b11a5f38427cb6873331b2d9b8f9d7618749a1a
anasks19 <anasks1999@gmail.com>
2023-09-07 06:27:11 -0700

    Write on stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char message[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
+}
