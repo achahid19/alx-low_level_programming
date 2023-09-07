acfadd112653bea29d12de9544a0e33b05bc2d00
anasks19 <anasks1999@gmail.com>
2023-09-07 06:31:16 -0700

    Write on stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char message[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", message);
+	return (0);
+}
