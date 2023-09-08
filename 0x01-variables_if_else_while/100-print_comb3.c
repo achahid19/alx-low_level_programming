4734d1d19e1a2256af62ea5ac54728634bb3025c
anasks19 <anasks1999@gmail.com>
2023-09-08 08:33:14 -0700

    print comb3:

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	digit1 = 0;
+	while (digit1 < 9)
+	{
+		digit2 = digit1 + 1;
+		while (digit2 < 10)
+		{
+			putchar(digit1 + 48);
+			putchar(digit2 + 48);
+			if (digit1 == 8)
+				break;
+			putchar(',');
+			putchar(' ');
+			digit2++;
+		}
+		digit1++;
+	}
+	return (0);
+}
