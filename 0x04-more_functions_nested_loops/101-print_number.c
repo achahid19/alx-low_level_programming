1c069cfd33e028addef82e965bb0a3b3819d8788
Anasks19 <anasks1999@gmail.com>
2023-09-14 19:20:11 +0000

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0)
+	{
+		if (n < 10)
+			_putchar(n + 48);
+		else if (n >= 10)
+		{
+			print_number(n / 10);
+			print_number(n % 10);
+		}
+	}
+}
