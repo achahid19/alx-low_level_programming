cdcf436ce9f1aeaf2da558b79c8fe6454d41458b
Anasks19 <anasks1999@gmail.com>
2023-09-14 15:04:32 +0000

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 1;
+	while (num <= 100)
+	{
+		if (num % 3 == 0)
+			printf("Fizz");
+		if (num % 5 == 0)
+			printf("Buzz");
+		else if (num % 3 != 0)
+			printf("%i", num);
+		printf(" ");
+		num++;
+	}
+	printf("\n");
+}
