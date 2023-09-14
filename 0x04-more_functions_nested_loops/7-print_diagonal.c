575de867dae31f6786859fe7d8555a4ece07c0de
Anasks19 <anasks1999@gmail.com>
2023-09-14 14:28:42 +0000

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on stdout
+ * @n: numbers of times '\' should be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	char draw_diagonal;
+	int count;
+	int add;
+
+	draw_diagonal = '\\';
+	count = 0;
+	add = 0;
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			while(count > 0)
+			{
+				_putchar(' ');
+				count--;
+			}
+			_putchar(draw_diagonal);
+			_putchar('\n');
+			add++;
+			count += add;
+			n--;
+		}
+	}
+	else if (n <= 0)
+		_putchar('\n');
+}
