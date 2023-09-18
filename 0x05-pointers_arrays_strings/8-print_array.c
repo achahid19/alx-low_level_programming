2ce4dc7d7b601567b28648a9f9e316d55754060c
Anasks19 <anasks1999@gmail.com>
2023-09-18 16:02:23 +0000

    poitners

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,34 @@ void print_array(int *a, int n)
 {
 	int count = 0;
 
-	while (count <= n)
-		printf("%d, ", *(a + count++));
+	while (count < n)
+	{
+		printf("%d", *(a + count));
+		if (count == n - 1)
+			break;
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		count++;
+	}
 	printf("\n");
+}
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[5];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
+    return (0);
 }
