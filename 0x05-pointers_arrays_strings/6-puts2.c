0b63b3b0a12eb94442340f46fbe9d25f9f71c79b
Anasks19 <anasks1999@gmail.com>
2023-09-18 14:05:31 +0000

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,13 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while (str[count])
+	while (str[count] != '\0')
 	{
 		if (count % 2 != 0)
+		{
 			count++;
+			continue;
+		}
 		_putchar(str[count++]);
 	}
 	_putchar('\n');
