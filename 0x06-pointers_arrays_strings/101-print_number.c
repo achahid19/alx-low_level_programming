42727699776864ec98943b2adf56b3a26e2de824
Anasks19 <anasks1999@gmail.com>
2023-09-20 15:33:44 +0000

    More pointers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+			write(1, "-2147483648", 11);
+			return;
+		}
+		_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0)
+	{
+		if (n < 10)
+			_putchar(n + 48);
+		else if (n >= 10)
+		{
+			print_number(n / 10);
+			print_number(n % 10);
+		}
+	}
+}
