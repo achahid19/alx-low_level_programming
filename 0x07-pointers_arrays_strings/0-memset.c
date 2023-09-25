525495c44a9beb054f922278c4f78d463a9c77c6
Anasks19 <anasks1999@gmail.com>
2023-09-25 11:53:53 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed by s
+ * with the constant b
+ * @s: pointer to the buffer
+ * @b: constant byte
+ * @n: n bytes
+ * Return: pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		s[count] = b;
+		count++;
+	}
+	return (s);
+}
