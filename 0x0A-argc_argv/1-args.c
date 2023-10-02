bdf60efbfbaa1e3598ebaa798c7c9f7da51f8df1
Anasks19 <anasks1999@gmail.com>
2023-10-02 13:57:10 +0000

    arguments count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @ac: arguments count
+ * @av: arguments vector
+ * Return: Always 0.
+*/
+
+int main(int ac, char **av)
+{
+	(void)av;
+	_putchar(ac + 48);
+	_putchar('\n');
+	return (EXIT_SUCCESS);
+}
