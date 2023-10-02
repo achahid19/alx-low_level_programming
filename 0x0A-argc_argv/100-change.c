139e403e2bc1426afb72855b44d5dcdc9f610a20
Anasks19 <anasks1999@gmail.com>
2023-10-02 18:40:42 +0000

    give back change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,40 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
- * @ac: arguments count
- * @av: arguments vector
- * Return: Always 0.
-*/
-int main(int ac, char **av)
+ * main - entry point
+ * @argc: atgument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
 {
-	char *change[] = {"25", "10", "5", "2", "1"};
-	int cents, count, index = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int count = 0;
+	int change = 0;
+	int cents;
 
-	count = 0;
-	if (ac == 2)
+	if (argc != 2)
 	{
-		if (atoi(av[1]) < 0)
-			write(1, "0\n", 2);
-		else if (atoi(av[1]) >= 0)
-		{
-			cents = atoi(av[1]);
-			while (count < 5)
-			{
-				if (cents >= atoi(change[count]))
-				{
-					cents -= atoi(change[count]);
-					index++;
-				}
-				else
-					count++;
-			}
-			printf("%d", index);
-		}
-	}
-	else
-	{
-		write(1, "Error\n", 6);
+		printf("Error\n");
 		return (1);
 	}
-	return (EXIT_SUCCESS);
+	cents = atoi(argv[1]);
+	if (cents < 0)
+		printf("0\n");
+	else if (argc >= 2)
+	{
+		while (*(coins + count) != 0)
+		{
+			change += (cents / coins[count]);
+			cents %= coins[count];
+			if (cents == 0)
+			break;
+			count++;
+		}
+		printf("%d\n", change);
+	}
+
+	return (0);
 }
