e1e9b3e143485e7e898895477dcd8a9971e8f3dd
Anasks19 <anasks1999@gmail.com>
2023-10-03 14:51:41 +0000

    allocated memory for concatenated str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,27 +23,45 @@ int ft_len(char *str)
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int len, count, count1;
+	int len, count, count1, count2;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-	len = ft_len(s1) + ft_len(s2);
-	str = malloc(sizeof(*str) * len + 1);
-	if (str == NULL)
+	else if (s1 == NULL || s2 == NULL)
 	{
-		return (NULL);
-	}
-	for (count = 0; count <= len; count++)
-	{
-		if (s1[count] != '\0')
-			str[count] = s1[count];
+		if (s1 == NULL)
+		{
+			str = malloc(sizeof(*str) * ft_len(s2) + 1);
+			for (count2 = 0; count2 <= ft_len(s2); count2++)
+				str[count2] = s2[count2];
+		}
 		else
 		{
-			for (count1 = 0; count <= len; count1++, count++)
+			str = malloc(sizeof(*str) * ft_len(s1) + 1);
+			for (count2 = 0; count2 <= ft_len(s1); count2++)
+				str[count2] = s1[count2];
+		}
+	}
+	else if (!(s1 == NULL && s2 == NULL))
+	{
+		len = ft_len(s1) + ft_len(s2);
+		str = malloc(sizeof(*str) * len + 1);
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		for (count = 0; count <= len; count++)
+		{
+			if (s1[count] != '\0')
+				str[count] = s1[count];
+			else
 			{
-				str[count] = s2[count1];
+				for (count1 = 0; count <= len; count1++, count++)
+				{
+					str[count] = s2[count1];
+				}
 			}
 		}
 	}
