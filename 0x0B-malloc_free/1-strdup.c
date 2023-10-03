5d4cce0c3030011680d9ed5039f0592fc59b81c5
Anasks19 <anasks1999@gmail.com>
2023-10-03 12:24:45 +0000

    allocate memory for duplicated str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * ft_strlen - returns the length of a string
+ * @str: string
+ * Return: str's length
+*/
+int ft_strlen(char *str)
+{
+	/* base case */
+	if (*str == '\0')
+		return (0);
+	return (ft_strlen(str + 1) + 1);
+}
+
+/**
+ * _strdup - allocate newly space memory
+ * which contains a copy of a given string
+ * @str: string to copy
+ * Return: pointer to the duplicated string
+*/
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len, count;
+
+	len = ft_strlen(str);
+	ptr = malloc(sizeof(*ptr) * len + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (count = 0; count <= len; count++)
+	{
+		*(ptr + count) = *(str + count);
+	}
+	return (ptr);
+}
