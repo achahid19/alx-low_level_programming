68abbc8a2bf4c0cee0c2090ef01c5b81d9303bf4
Anasks19 <anasks1999@gmail.com>
2023-10-04 10:47:05 +0000

    allocate memory for 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate 2D array of ints
+ * @width: number of columns in the array
+ * @height: number of rows in the array
+ * Return: pointer to 2Darray
+*/
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int count1, count2;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(**grid) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (count1 = 0; count1 < height; count1++)
+	{
+		grid[count1] = malloc(sizeof(*grid) * width);
+		if (grid[count1] == NULL)
+			return (NULL);
+		for (count2 = 0; count2 < width; count2++)
+		{
+			grid[count1][count2] = 0;
+		}
+	}
+	return (grid);
+}
