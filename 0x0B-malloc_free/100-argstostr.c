7537482823f420570e0e7052d744b2396738d9b3
Anasks19 <anasks1999@gmail.com>
2023-10-04 12:28:51 +0000

    2D to 1D

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -52,24 +52,3 @@ char *argstostr(int ac, char **av)
 	str[str_count] = '\0';
 	return (str);
 }
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
-}
