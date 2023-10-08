55c053ed9d108c0cf691a75d864d2e482b71ee86
Anasks19 <anasks1999@gmail.com>
2023-10-08 19:09:14 +0000

    allocate array and concat n Bytes of s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,22 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int count, count1, len = 0;
-	char *str;
+	unsigned int len1, len2;
+	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		if (s1 == NULL)
-			s1 = "";
-		else
-			s2 = "";
-	}
-	len += strlen(s1);
-	if (n > strlen(s2))
-	{
-		n = strlen(s2);
-	}
-	str = malloc(sizeof(*str) + len + n + 1);
-	if (str == NULL)
-	{
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2)
+	n = len2;
+	concat = malloc(len1 + n + 1);
+	if (concat == NULL)
 		return (NULL);
-	}
-	for (count = 0; count < strlen(s1); count++)
-	{
-		str[count] = s1[count];
-	}
-	for (count1 = 0; count1 < n; count1++)
-	{
-		str[count + count1] = s2[count1];
-	}
-	str[count + count1] = '\0';
-	return (str);
+	memcpy(concat, s1, len1);
+	memcpy(concat + len1, s2, n);
+	concat[len1 + n] = '\0';
+	return (concat);
 }
