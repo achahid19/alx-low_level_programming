4ab3482a9ab7098f4471352ad36ba201f10e47bd
Anasks19 <anasks1999@gmail.com>
2023-10-11 11:56:16 +0000

    pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name by a callback to another function
+ * @name: name to print
+ * @f: pointer to function
+ * Return: void.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
