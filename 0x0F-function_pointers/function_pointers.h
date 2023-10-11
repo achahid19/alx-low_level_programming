a857c71bdeed98d429ed7de1f5dacf03d451012c
Anasks19 <anasks1999@gmail.com>
2023-10-11 11:56:43 +0000

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 void _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* MAIN_H */
