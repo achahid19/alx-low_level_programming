806db28b5a81ea006113f10db826fd9185230586
Anasks19 <anasks1999@gmail.com>
2023-10-12 10:39:07 +0000

    prints n arguments of a variadic func of type int

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints n numbers followed by new line
+ * @n: number of arguments passed to the function
+ * @separator: string to be printed between numbers
+ * Return: void.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int count;
+
+	va_start(nums, n); /* initialize arg list */
+	for (count = 0; count < n; count++)
+	{
+		printf("%d", va_arg(nums, int)); /* print the next value */
+		if (separator != NULL && count != n - 1)
+			printf("%s", separator);
+	}
+	va_end(nums); /* clean up */
+	printf("\n");
+}
+
+int main(void)
+{
+	print_numbers(", ", 3, 5, 4, 10);
+	return (0);
+}
