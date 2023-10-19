6dfa85515a32785df875087b2e2aba03633f02c0
anasks19 <anasks1999@gmail.com>
2023-10-19 17:39:09 +0000

    print singly listed list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,13 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-	for (count = 0; h->next != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
 	}
 	return (count);
 }
