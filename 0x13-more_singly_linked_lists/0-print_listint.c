1e85bf398bab400a427841dc479d2c334f50f300
anasks19 <anasks1999@gmail.com>
2023-10-23 10:09:55 +0000

    print int elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next; 
+	}
+	return (count);
+}
