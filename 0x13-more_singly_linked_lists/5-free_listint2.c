e8388ea1cdec4e232c97d22cd6c932c3e781e265
anasks19 <anasks1999@gmail.com>
2023-10-24 10:27:08 +0000

    frees nodes of a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a singly linked list
+ * @head: double pointer to a pointer to head (first node)
+ * Return: void.
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL || head == NULL)
+	{
+		return;
+	}
+	temp = *head;
+	for (; *head != NULL;)
+	{
+		temp = (*head)->next;
+		free(head);
+		*head = temp;
+	}
+}
