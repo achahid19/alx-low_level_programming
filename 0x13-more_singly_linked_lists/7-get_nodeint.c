4c82d6075478a0366f75a9ee419cc799fa6f5fca
anasks19 <anasks1999@gmail.com>
2023-10-24 11:00:39 +0000

    return a node in a given index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return a node in an given index
+ * @index: the position of the node in the linked list, starting at 0.
+ * Return: node in index or NULL if doesn't exist. 
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	for (count = 0; count < index; count++)
+	{
+		if (head->next != NULL)
+			head = head->next;
+		else
+			return (NULL);
+	}
+	return (head);
+}
