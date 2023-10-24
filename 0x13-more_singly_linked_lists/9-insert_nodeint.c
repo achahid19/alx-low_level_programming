d3f5f70791887767ad908d3cb644dbde29bfc7c0
anasks19 <anasks1999@gmail.com>
2023-10-24 17:23:52 +0000

    inserts a new node in a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -53,7 +53,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if ((*head) || (*head)->next != NULL)
 		{
 			*head = (*head)->next;
-			if (*head == NULL)
+			if ((*head)->next == NULL)
 				return (NULL);
 		}
 		else
