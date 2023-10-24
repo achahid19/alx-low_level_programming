97528240f00a95338b67143c557e05da5457ee78
anasks19 <anasks1999@gmail.com>
2023-10-24 18:26:43 +0000

    delete a node at index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -49,7 +49,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if ((*head)->next)
 		{
 			*head = (*head)->next;
-			if (*head == NULL && count < index - 1)
+			if (*head == NULL)
 				return (-1);
 		}
 		else
