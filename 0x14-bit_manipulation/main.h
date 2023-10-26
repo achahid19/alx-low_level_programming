30d853153a7f8bab5b63da05dc80a9d04042377c
anasks19 <anasks1999@gmail.com>
2023-10-26 12:20:51 +0000

    update header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,9 @@
 #include <string.h>
 
 unsigned int ft_pow(int base, int expo);
+void _putchar(char c);
+
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
