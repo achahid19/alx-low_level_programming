8c9157c6bb0cfa3b2f3578a3bcde335cc5e4b0b2
anasks19 <anasks1999@gmail.com>
2023-10-26 15:22:11 +0000

    update header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
 unsigned int ft_pow(int base, int expo);
@@ -11,5 +10,6 @@ void _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
