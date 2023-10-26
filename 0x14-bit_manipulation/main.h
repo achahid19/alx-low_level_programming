b8ddc45661b10b353b5879ead6af8f46a36e6d43
anasks19 <anasks1999@gmail.com>
2023-10-26 11:41:48 +0000

    header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+unsigned int ft_pow(int base, int expo);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
